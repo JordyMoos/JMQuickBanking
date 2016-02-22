07dc832988f448ba3d0aad378c5d330dd956fbdf
Jordy Moos <jordymoos@gmail.com>
2016-02-22 22:28:52 +0100

    Initial setup

---
+++ JMQuickBanking.lua
@@ -0,0 +1,74 @@
+
+---
+--- JMQuickBanking
+---
+
+--[[
+
+    Variable declaration
+
+ ]]
+
+---
+-- @field name
+-- @field savedVariablesName
+--
+local Config = {
+    name = 'JMQuickBanking',
+    savedVariablesName = 'JMQuickBankingSavedVariables',
+}
+
+local SavedVariables = {}
+
+--[[
+
+    Initialize
+
+ ]]
+
+---
+-- Start of the addon
+--
+local function Initialize()
+    -- Load the saved variables
+    SavedVariables = ZO_SavedVars:NewAccountWide(Config.savedVariablesName, 1, nil, {
+        --buyList = {},
+    })
+    --BuyList = SavedVariables.buyList
+
+    
+end
+
+
+--[[
+
+    Api
+
+ ]]
+
+---
+-- Making some functions public
+--
+JMQuickBanking = {
+    -- No API calls yet
+}
+
+--[[
+
+    Events
+
+ ]]
+
+--- Adding the initialize handler
+EVENT_MANAGER:RegisterForEvent(
+    Config.name,
+    EVENT_ADD_ON_LOADED,
+    function (event, addonName)
+        if addonName ~= Config.name then
+            return
+        end
+
+        Initialize()
+        EVENT_MANAGER:UnregisterForEvent(Config.name, EVENT_ADD_ON_LOADED)
+    end
+)
